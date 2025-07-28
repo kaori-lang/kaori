7127731ee8f57bfe5ef6340a3c8833cef238f81f
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-07-28 00:24:43 -0300

    created environment

--- src/compiler/semantic/resolver.rs
+++ src/compiler/semantic/resolver.rs
@@ -1,20 +1,68 @@
 use crate::compiler::syntax::{
-    ast_node::ASTNode, expression_ast::ExpressionAST, statement_ast::StatementAST,
-    type_ast::TypeAST,
+    ast_node::ASTNode, declaration_ast::DeclarationAST, expression_ast::ExpressionAST,
+    statement_ast::StatementAST, type_ast::TypeAST,
 };
 
-use super::visitor::Visitor;
+use super::{environment::Environment, visitor::Visitor};
 
 pub struct Resolver {
     declarations: Vec<ASTNode>,
+    environment: Environment<String>,
+}
+
+pub struct Resolution {
+    offset: usize,
+    local: bool,
 }
 
 impl Resolver {
     fn new(declarations: Vec<ASTNode>) -> Self {
-        Self { declarations }
+        Self {
+            declarations,
+            environment: Environment::new(),
+        }
+    }
+
+    fn search_current_scope(&mut self, identifier: String) -> Option<Resolution> {
+        let start = self.environment.stack.len() - 1;
+        let end = self.environment.scopes_start.last().unwrap();
+
+        for i in start..=*end {
+            if identifier == self.environment.stack[i] {
+                let offset = return Some(i);
+            }
+        }
+
+        return None;
+    }
+
+    fn search(&mut self, identifier: String) -> Option<usize> {
+        let start = self.environment.stack.len() - 1;
+        let end = 0;
+
+        for i in start..=end {
+            if identifier == self.environment.stack[i] {
+                return Some(i);
+            }
+        }
+
+        return None;
     }
 }
-impl Visitor<TypeAST> for Resolver {
+
+impl Visitor<()> for Resolver {
+    fn visit_declaration(&self, declaration: DeclarationAST) {
+        match declaration {
+            DeclarationAST::Variable {
+                identifier,
+                right,
+                ty,
+                span,
+                offset,
+            } => (),
+        }
+    }
+
     fn visit_statement(&self, statement: StatementAST) {
         match statement {
             StatementAST::Expression { expression, span } => {
@@ -25,12 +73,9 @@ impl Visitor<TypeAST> for Resolver {
         }
     }
 
-    fn visit_expression(&self, expression: ExpressionAST) -> TypeAST {
+    fn visit_expression(&self, expression: ExpressionAST) {
         match expression {
-            ExpressionAST::NumberLiteral(number) => TypeAST::Number,
-            ExpressionAST::BooleanLiteral(bool) => TypeAST::Boolean,
-            ExpressionAST::StringLiteral(str) => TypeAST::String,
-            ExpressionAST::Identifier(identifier) => TypeAST::String,
+            ExpressionAST::Identifier(identifier) => {}
             ExpressionAST::Assign { identifier, right } => TypeAST::String,
             ExpressionAST::Binary {
                 operator,
@@ -40,9 +85,9 @@ impl Visitor<TypeAST> for Resolver {
                 let left = self.visit_expression(*left);
                 let right = self.visit_expression(*right);
 
-                TypeAST::Boolean
+                ()
             }
-            _ => TypeAST::Boolean,
+            _ => (),
         }
     }
 }
--- src/compiler/semantic/visitor.rs
+++ src/compiler/semantic/visitor.rs
@@ -1,8 +1,10 @@
 use crate::compiler::syntax::{
-    ast_node::ASTNode, expression_ast::ExpressionAST, statement_ast::StatementAST,
+    ast_node::ASTNode, declaration_ast::DeclarationAST, expression_ast::ExpressionAST,
+    statement_ast::StatementAST,
 };
 
 pub trait Visitor<T> {
     fn visit_expression(&self, expression: ExpressionAST) -> T;
     fn visit_statement(&self, statement: StatementAST);
+    fn visit_declaration(&self, declaration: DeclarationAST);
 }
