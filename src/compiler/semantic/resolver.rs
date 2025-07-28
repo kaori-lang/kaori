aeb4e45244fd163c4e81b7429856dd018e292926
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-07-28 13:40:55 -0300

    added resolver implementations

--- src/compiler/semantic/resolver.rs
+++ src/compiler/semantic/resolver.rs
@@ -4,10 +4,10 @@ use crate::{
         lexer::span::Span,
         syntax::{
             ast_node::ASTNode, declaration_ast::DeclarationAST, expression_ast::ExpressionAST,
-            statement_ast::StatementAST, type_ast::TypeAST,
+            statement_ast::StatementAST,
         },
     },
-    error::compilation_error::{self, CompilationError},
+    error::compilation_error::CompilationError,
 };
 
 use super::{environment::Environment, visitor::Visitor};
@@ -80,6 +80,13 @@ impl Resolver {
 }
 
 impl Visitor<()> for Resolver {
+    fn visit_ast_node(&mut self, ast_node: ASTNode) -> Result<(), CompilationError> {
+        match ast_node {
+            ASTNode::Declaration(declaration) => self.visit_declaration(declaration),
+            ASTNode::Statement(statement) => self.visit_statement(statement),
+        }
+    }
+
     fn visit_declaration(&mut self, declaration: DeclarationAST) -> Result<(), CompilationError> {
         match declaration {
             DeclarationAST::Variable {
@@ -105,17 +112,80 @@ impl Visitor<()> for Resolver {
         }
     }
 
-    fn visit_statement(&self, statement: StatementAST) -> Result<(), CompilationError> {
+    fn visit_statement(&mut self, statement: StatementAST) -> Result<(), CompilationError> {
         match statement {
-            StatementAST::Expression { expression, span } => {
+            StatementAST::Expression(expression) => {
                 self.visit_expression(*expression)?;
+                Ok(())
+            }
+            StatementAST::Print { expression, .. } => {
+                self.visit_expression(*expression)?;
+                Ok(())
+            }
+            StatementAST::Block { declarations, .. } => {
+                self.environment.enter_scope();
+
+                for declaration in declarations {
+                    self.visit_ast_node(declaration)?;
+                }
+
+                self.environment.exit_scope();
+
+                Ok(())
+            }
+            StatementAST::If {
+                condition,
+                then_branch,
+                else_branch,
+                ..
+            } => {
+                self.visit_expression(*condition)?;
+                self.visit_statement(*then_branch)?;
+
+                if let Some(branch) = else_branch {
+                    self.visit_statement(*branch)?;
+                }
+
                 Ok(())
             }
             _ => Ok(()),
         }
     }
 
-    fn visit_expression(&self, expression: ExpressionAST) -> Result<(), CompilationError> {
-        Ok(())
+    fn visit_expression(&mut self, expression: ExpressionAST) -> Result<(), CompilationError> {
+        match expression {
+            ExpressionAST::Assign {
+                identifier,
+                right,
+                span,
+            } => {
+                self.visit_expression(*right)?;
+
+                let Some(_) = self.search(&identifier) else {
+                    return Err(compilation_error!(span, "{} is not declared", identifier));
+                };
+
+                Ok(())
+            }
+            ExpressionAST::Binary { left, right, .. } => {
+                self.visit_expression(*left)?;
+                self.visit_expression(*right)?;
+
+                Ok(())
+            }
+            ExpressionAST::Unary { right, .. } => {
+                self.visit_expression(*right)?;
+
+                Ok(())
+            }
+            ExpressionAST::Identifier(identifier, span) => {
+                let Some(_) = self.search(&identifier) else {
+                    return Err(compilation_error!(span, "{} is not declared", identifier));
+                };
+
+                Ok(())
+            }
+            _ => Ok(()),
+        }
     }
 }
--- src/compiler/semantic/visitor.rs
+++ src/compiler/semantic/visitor.rs
@@ -1,12 +1,14 @@
 use crate::{
     compiler::syntax::{
-        declaration_ast::DeclarationAST, expression_ast::ExpressionAST, statement_ast::StatementAST,
+        ast_node::ASTNode, declaration_ast::DeclarationAST, expression_ast::ExpressionAST,
+        statement_ast::StatementAST,
     },
     error::compilation_error::CompilationError,
 };
 
 pub trait Visitor<T> {
-    fn visit_expression(&self, expression: ExpressionAST) -> Result<T, CompilationError>;
-    fn visit_statement(&self, statement: StatementAST) -> Result<(), CompilationError>;
+    fn visit_ast_node(&mut self, ast_node: ASTNode) -> Result<(), CompilationError>;
     fn visit_declaration(&mut self, declaration: DeclarationAST) -> Result<(), CompilationError>;
+    fn visit_statement(&mut self, statement: StatementAST) -> Result<(), CompilationError>;
+    fn visit_expression(&mut self, expression: ExpressionAST) -> Result<T, CompilationError>;
 }
