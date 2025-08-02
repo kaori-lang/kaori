b16912b9d31b1393c62ccc7d13efeb6cae4cfac1
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-08-02 01:22:04 -0300

    removed bound check loading const from constant pool

--- src/backend/vm/kaori_vm.rs
+++ src/backend/vm/kaori_vm.rs
@@ -122,9 +122,13 @@ impl KaoriVM {
                     println!("{:?}", value.as_number());
                 }
                 Opcode::LoadConst => {
-                    let value = self.bytecode.constant_pool[instruction.operand];
+                    let value = unsafe {
+                        self.bytecode
+                            .constant_pool
+                            .get_unchecked(instruction.operand)
+                    };
 
-                    value_stack.push(value);
+                    value_stack.push(*value);
                 }
                 Opcode::Declare => {
                     let value = value_stack.pop();
