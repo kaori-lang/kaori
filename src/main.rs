c07c75952ffad21ab4bf0c29f86a05c5d7655802
drkwitht <derektan3510@gmail.com>
2025-09-06 12:48:33 -0700

    add(prgm-args): added support for passed source paths to exec.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
+use std::env::args;
+use std::process::ExitCode;
+
+// TODO: remove this line suppression after using the import.
+#[allow(unused_imports)]
 use std::{fs, time::Instant};
 
 use kaori::{ error::kaori_error::KaoriError, frontend::parse_and_analyze::parse_and_analyze};
 
-fn main() {
-    if let Ok(source) = fs::read_to_string("src/code/main.kaori") {
+fn main() -> ExitCode {
+    let source_to_run = args().nth(1);
+
+    if source_to_run.is_none() {
+        eprintln!("Error: No path was found for the program's source!");
+        return ExitCode::FAILURE;
+    }
+
+    let source_path = source_to_run.unwrap();
+
+    if let Ok(source) = fs::read_to_string(source_path) {
         if let Err(err) = run_program(source.clone()) {
             err.report(&source);
+            return ExitCode::FAILURE;
         }
+
+        return ExitCode::SUCCESS;
     }
+
+    eprintln!("Error: Could not read the file by the given path.");
+    ExitCode::FAILURE
 }
 
+// TODO: remove the lint suppressions after using unused variables.
+#[allow(unused_variables)]
 pub fn run_program(source: String) -> Result<(), KaoriError> {
+    #[allow(clippy::let_unit_value)]
     let resolved_declarations = parse_and_analyze(source)?;
 
     /*  let mut bytecode = Bytecode::default();
