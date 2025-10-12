d3e4731d72be4e120b6ab66157623e5a4ac6dedb
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-10-12 19:41:24 -0300

    .

--- src/virtual_machine/kaori_vm.rs
+++ src/virtual_machine/kaori_vm.rs
@@ -2,7 +2,25 @@ use std::hint::unreachable_unchecked;
 
 use crate::bytecode::{instruction::Instruction, value::Value};
 
-use super::function_frame::FunctionFrame;
+pub struct FunctionFrame {
+    pub registers: *mut Value,
+    pub return_address: *const Instruction,
+    pub return_register: i16,
+}
+
+impl FunctionFrame {
+    pub fn new(
+        registers: *mut Value,
+        return_address: *const Instruction,
+        return_register: i16,
+    ) -> Self {
+        Self {
+            registers,
+            return_address,
+            return_register,
+        }
+    }
+}
 
 type InstructionHandler = fn(&mut VMContext, ip: *const Instruction);
 pub struct VMContext {
