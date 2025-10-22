6b52634b112c3143e888b3dfc6166bd143e299d7
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-10-22 02:35:38 -0300

    fixed vm bug on return instruction

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn main() -> ExitCode {
 */
 
 fn main() {
-    let source_to_run = "test_suite/iterative_fib.kr";
+    let source_to_run = "test_suite/recursive_fib.kr";
 
     match fs::read_to_string(source_to_run) {
         Ok(source) => {
