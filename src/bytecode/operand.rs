7879978c581b5ac728e403e37683bbcaa54c1b5a
washingtonfbueno <washingtonfbueno@hotmail.com>
2026-04-17 02:21:20 -0300

    .

--- src/bytecode/operand.rs
+++ src/bytecode/operand.rs
@@ -1,35 +1,10 @@
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy)]
 pub enum Operand {
+    Constant(u8),
     Register(u8),
-    BoolImm(bool),
-    NilImm,
-    NumberImm(u16),
 }
 
 impl Operand {
-    const SCALE: f64 = 256.0;
-
-    pub fn from_bool(value: bool) -> Self {
-        Self::BoolImm(value)
-    }
-
-    pub fn nil() -> Self {
-        Self::NilImm
-    }
-
-    #[inline(always)]
-    pub fn from_f64(value: f64) -> Option<Self> {
-        if !value.is_finite() {
-            return None;
-        }
-        let scaled = value * Self::SCALE;
-        if scaled < i16::MIN as f64 || scaled > i16::MAX as f64 {
-            return None;
-        }
-        let fixed = scaled.round() as i16;
-        Some(Self::NumberImm(fixed as u16))
-    }
-
     pub fn unwrap_register(self) -> u8 {
         if let Operand::Register(value) = self {
             value
@@ -38,33 +13,11 @@ impl Operand {
         }
     }
 
-    pub fn unwrap_number(self) -> f64 {
-        if let Operand::NumberImm(bits) = self {
-            bits as i16 as f64 / Self::SCALE
-        } else {
-            unreachable!("Expected a number to be unwrapped")
-        }
-    }
-
-    pub fn unwrap_bool(self) -> bool {
-        if let Operand::BoolImm(value) = self {
+    pub fn unwrap_constant(self) -> u8 {
+        if let Operand::Constant(value) = self {
             value
         } else {
-            unreachable!("Expected a bool to be unwrapped")
+            unreachable!("Expected a constant to be unwrapped")
         }
-    }
-
-    pub fn operand_to_imm(&self) -> u16 {
-        match *self {
-            Operand::NumberImm(bits) => bits,
-            Operand::BoolImm(true) => 1,
-            Operand::BoolImm(false) => 0,
-            Operand::NilImm => u16::MAX,
-            Operand::Register(_) => unreachable!("operand_to_imm called with a register"),
-        }
-    }
-
-    pub fn is_nil(self) -> bool {
-        matches!(self, Operand::NilImm)
     }
 }
--- tests_bench/recursive_fib.lua
+++ tests_bench/recursive_fib.lua
@@ -8,3 +8,29 @@ end
 
 fib(30)
 
+main <tests_bench/recursive_fib.lua:0,0> (6 instructions at 0000000000ae8cb0)
+0+ params, 3 slots, 1 upvalue, 1 local, 0 constants, 1 function
+        1       [1]     VARARGPREP      0
+        2       [6]     CLOSURE         0 0     ; 0000000000ae8ef0
+        3       [9]     MOVE            1 0
+        4       [9]     LOADI           2 30
+        5       [9]     CALL            1 2 1   ; 1 in 0 out
+        6       [9]     RETURN          1 1 1k  ; 0 out
+
+function <tests_bench/recursive_fib.lua:1,6> (15 instructions at 0000000000ae8ef0)
+1 param, 4 slots, 1 upvalue, 1 local, 0 constants, 0 functions
+        1       [2]     LTI             0 2 0
+        2       [2]     JMP             1       ; to 4
+        3       [3]     RETURN1         0
+        4       [5]     GETUPVAL        1 0     ; fib
+        5       [5]     ADDI            2 0 -1
+        6       [5]     MMBINI          0 1 7 0 ; __sub
+        7       [5]     CALL            1 2 2   ; 1 in 1 out
+        8       [5]     GETUPVAL        2 0     ; fib
+        9       [5]     ADDI            3 0 -2
+        10      [5]     MMBINI          0 2 7 0 ; __sub
+        11      [5]     CALL            2 2 2   ; 1 in 1 out
+        12      [5]     ADD             1 1 2
+        13      [5]     MMBIN           1 2 6   ; __add
+        14      [5]     RETURN1         1
+        15      [6]     RETURN0
