f23899d1dabf19d67f70addaae59833b51b460e0
washingtonfbueno <washingtonfbueno@hotmail.com>
2026-04-25 18:17:39 -0300

    removed hir

--- src/hir/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod expr;
-
-pub mod node_id;
-pub mod resolver;
-pub mod symbol;
-pub mod symbol_table;
--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@
 pub mod ast;
 pub mod bytecode;
 pub mod error;
-pub mod hir;
 pub mod lexer;
 pub mod program;
 pub mod runtime;
