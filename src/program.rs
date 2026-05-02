577c11e11b46f433ded0c8fe72799c69437e1482
washingtonfbueno <washingtonfbueno@hotmail.com>
2026-05-02 20:43:13 -0300

    .

--- src/program.rs
+++ src/program.rs
@@ -15,8 +15,8 @@ use crate::{
 
 pub static INTERNER: LazyLock<Mutex<StringInterner>> =
     LazyLock::new(|| Mutex::new(StringInterner::default()));
-pub static CONSTANT_POOL: OnceLock<Box<[Value]>> = OnceLock::new();
-pub static FUNCTIONS: OnceLock<Box<[Function]>> = OnceLock::new();
+pub static CONSTANT_POOL: OnceLock<Vec<Value>> = OnceLock::new();
+pub static FUNCTIONS: OnceLock<Vec<Function>> = OnceLock::new();
 
 pub fn compile_source_code(source: &str) -> Result<(Vec<Function>, Vec<Value>), Error> {
     let tokens = Token::lexer(source).spanned();
@@ -38,8 +38,8 @@ pub fn compile_source_code(source: &str) -> Result<(Vec<Function>, Vec<Value>), 
 pub fn run_program(source: &str) -> Result<(), Error> {
     let (bytecode, constants) = compile_source_code(source)?;
 
-    FUNCTIONS.set(bytecode.into_boxed_slice()).unwrap();
-    CONSTANT_POOL.set(constants.into_boxed_slice()).unwrap();
+    FUNCTIONS.set(bytecode).unwrap();
+    CONSTANT_POOL.set(constants).unwrap();
 
     let start = Instant::now();
 
